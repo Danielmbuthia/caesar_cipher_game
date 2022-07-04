7e49e6c118ab761e1b08838344f88feca0e4e791
daniel mbuthia <danielwandurua@gmail.com>
2022-07-04 23:02:30 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+def encrypt(text,shift):
+    encrypted_word =""
+    for letter in text:
+        position = alphabet.index(letter)
+        new_position = position + shift
+        if new_position > 25: # index start from 0
+            new_position -= 26 # position is 26 len of list 
+        new_letter = alphabet[new_position]
+        encrypted_word += new_letter
+    print(f"The encoded text is {encrypted_word}") 
+
+def decrypt(text,shift):
+    decrypted_word =""
+    for letter in text:
+        position = alphabet.index(letter) - shift
+        if position < 0: # index start from 0
+            position += 26 # position is 26 len of list 
+        new_letter = alphabet[position]
+        decrypted_word += new_letter
+    print(f"The decoded text is {decrypted_word}")
+if direction ==  "encode": 
+    encrypt(text,shift)
+elif direction ==  "decrypt":
+    decrypt(text,shift)
